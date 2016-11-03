f399805ea93b098f5a77c26bf8e7d8752fc05bc0
Jonathan Stahl <joni@stahlfamily.ch>
2016-11-03 12:07:22 +0100

    added class Table

---
+++ CliMeter/src/ch/uzh/ifi/rerg/se16_climeter/client/Table.java
@@ -0,0 +1,13 @@
+package ch.uzh.ifi.rerg.se16_climeter.client;
+
+public class Table implements Exportable{
+	
+	
+
+	@Override
+	public void export() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
