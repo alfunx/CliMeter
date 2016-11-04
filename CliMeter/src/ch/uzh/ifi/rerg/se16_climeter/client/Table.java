d1584b546193c93759a3a4562653ecbf7cdbc8ae
Jonathan Stahl <joni@stahlfamily.ch>
2016-11-04 21:19:26 +0100

    cellTable

--- CliMeter/src/ch/uzh/ifi/rerg/se16_climeter/client/Table.java
+++ CliMeter/src/ch/uzh/ifi/rerg/se16_climeter/client/Table.java
@@ -1,14 +1,18 @@
 package ch.uzh.ifi.rerg.se16_climeter.client;
 
 import java.util.Date;
-import java.text.DateFormat;
-import com.google.gwt.i18n.client.NumberFormat;
 
-import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.cell.client.DateCell;
+import com.google.gwt.user.cellview.client.CellTable;
+import com.google.gwt.user.cellview.client.Column;
+import com.google.gwt.user.cellview.client.HasKeyboardSelectionPolicy.KeyboardSelectionPolicy;
+
+import java.text.DateFormat;
+
 
 public class Table extends Visualisation implements Exportable{
 	
-	private FlexTable table;
+	private CellTable<Data> table;
 	
 	
 	public Table(){
@@ -19,30 +23,41 @@ public class Table extends Visualisation implements Exportable{
 	 * init a new table 
 	 */
 	private void initTable() {
-		table  = new FlexTable();
+		table  = new CellTable<Data>();
+		table.setKeyboardSelectionPolicy(KeyboardSelectionPolicy.ENABLED);
 		
 		// create header cells
 		
-		table.setText(0, 0 , "Date");
-		table.setText(0, 1 , "AveTemperature");
-		table.setText(0, 2 , "Uncertainty");
-		table.setText(0, 3 , "City");
-		table.setText(0, 4 , "Country");
-		table.setText(0, 5 , "Latitude");
-		table.setText(0, 6 , "Longitude");
+		 DateCell dateCell = new DateCell();
+		 Column<Data, Date> dateColumn = new Column<Data, Date>(dateCell) {
+			 
+			 @Override
+			 public Date getValue(Data object) {
+				 return object.getDate();
+		     }
+		  };
+		  
+		  table.addColumn(dateColumn, "Date");
+		  
+		  DateCell dateCell = new DateCell();
+			 Column<Data, Date> dateColumn = new Column<Data, Date>(dateCell) {
+				 
+				 @Override
+				 public Date getValue(Data object) {
+					 return object.getDate();
+			     }
+			  };
+			  
+			  table.addColumn(dateColumn, "Date");
+		    
+		  
+		
+		
+		
+		
 		
 		// add styles
 		
-		table.setStyleName("table");
-		table.getRowFormatter().addStyleName(0, "tableHeader");
-		table.getCellFormatter().addStyleName(0, 0, "tableNumericColumn");
-		table.getCellFormatter().addStyleName(0, 1, "tableNumericColumn");
-		table.getCellFormatter().addStyleName(0, 2, "tableNumericColumn");
-		table.getCellFormatter().addStyleName(0, 3, "tableNumericColumn");
-		table.getCellFormatter().addStyleName(0, 4, "tableNumericColumn");
-		table.getCellFormatter().addStyleName(0, 5, "tableNumericColumn");
-		table.getCellFormatter().addStyleName(0, 6, "tableNumericColumn");
-		table.getCellFormatter().addStyleName(0, 7, "tableNumericColumn");
 		
 		
 		panel.add(table);
@@ -64,18 +79,7 @@ public class Table extends Visualisation implements Exportable{
 		double latitude = data.getLatitude();
 		double longitude = data.getLongitude();
 		
-		// fill in information into cells
-		
-		int row = table.getRowCount();
-		
-//		table.setText(row, 0, date.toString());
-//		table.setText(row, 1, NumberFormat.);
-//		table.setText(row, 2, date.toString());
-//		table.setText(row, 3, date.toString());
-//		table.setText(row, 4, date.toString());
-//		table.setText(row, 5, date.toString());
-//		table.setText(row, 6, date.toString());
-		
+
 		
 		
 	}
