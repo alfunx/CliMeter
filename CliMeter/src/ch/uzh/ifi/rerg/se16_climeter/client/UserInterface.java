3e76e4a59169c8cc1ec9d9942e19da1af0f1d2a3
timolex <timo.surbeck@gmail.com>
2016-11-07 18:29:10 +0100

    added documentation, small restructuring (only in class UI)

--- CliMeter/src/ch/uzh/ifi/rerg/se16_climeter/client/UserInterface.java
+++ CliMeter/src/ch/uzh/ifi/rerg/se16_climeter/client/UserInterface.java
@@ -1,53 +1,64 @@
 package ch.uzh.ifi.rerg.se16_climeter.client;
 
-
 import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TabLayoutPanel;
-import com.google.gwt.user.client.ui.VerticalPanel;
 
+/**
+ * The class createGUI creates the tab structured GUI and adds data source
+ * information to the visible part of the web site.
+ * 
+ * @author Timo Surbeck
+ * @history 2016-11-8 First Version 2016-11-10 Several layout improvements
+ * @version 2016-11-07 Added data source information
+ * 
+ * @responsibilities This class holds several children-objects of the abstract
+ *                   class Visualisation.
+ */
 
 public class UserInterface {
+
+	private final String dataSource = "Berkeley Earth";
+	private final String sourceURL = "http://www.berkeleyearth.org/";
+	
+	private TabLayoutPanel tabs = new TabLayoutPanel(20, Unit.PT);;
+	// private Visualisation map;
+	// private Visualisation table;
+	// private FilterMenu filterMenu;
+	
 	/**
-	 * The message displayed to the user when the server cannot be reached or
-	 * returns an error.
+	 * Creates the tab structured GUI and displays the data source information.
+	 * Requires an already existing TabLayoutPanel object called tabs.
+	 * @pre	-
+	 * @post -
+	 * @param -
+	 * @return - 
 	 */
-	/*private static final String SERVER_ERROR = "An error occurred while "
-			+ "attempting to contact the server. Please check your network " + "connection and try again.";
-	*/
-	
-	private TabLayoutPanel tabs;
-	//private Visualisation map;
-	//private Visualisation table;
-	//private Filter filter;
-	
-	
 	public void createGUI() {
-		tabs = new TabLayoutPanel(20, Unit.PT);
 		
 		tabs.add(new Label("Map"), "Map");
 		tabs.add(new Label("Table"), "Table");
 		tabs.add(new Label("Filter"), "Filter");
-		
+
 		RootPanel.get("tabContainer").add(tabs);
 
 		tabs.setHeight("400px");
-		
+
 		HorizontalPanel sourceContainer = new HorizontalPanel();
 		sourceContainer.addStyleName("sourceContainer");
-		
+
 		Label sourceInfo = new Label("Source of raw data:");
-		Anchor sourceLink = new Anchor("Berkeley Earth", "http://www.berkeleyearth.org/");
+		Anchor sourceLink = new Anchor(dataSource, sourceURL);
 		sourceLink.setTarget("_blank");
-		
+
 		sourceInfo.addStyleName("sourceInfo");
-		
+
 		sourceContainer.add(sourceInfo);
 		sourceContainer.add(sourceLink);
-		
+
 		RootPanel.get("sourceContainer").add(sourceContainer);
 
 	}
