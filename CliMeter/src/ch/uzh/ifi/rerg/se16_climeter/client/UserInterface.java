27b82ed378d7c95908ea403d731a626de9b31172
joechi <joachimbaumann1@gmail.com>
2016-11-10 14:50:53 +0100

    filterMenu implemented

--- CliMeter/src/ch/uzh/ifi/rerg/se16_climeter/client/UserInterface.java
+++ CliMeter/src/ch/uzh/ifi/rerg/se16_climeter/client/UserInterface.java
@@ -14,21 +14,23 @@ import com.google.gwt.user.client.ui.TabLayoutPanel;
  * @author Timo Surbeck
  * @history 2016-11-01 First Version 
  * 			2016-11-03 Several layout improvements
- * @version 2016-11-07 Added data source information
+ * 			2016-11-07 Added data source information
+ * 			2016-11-09 added FilterMenu (Joachim Baumann)
+ * @version 2016-11-07 
  * 
  * @responsibilities This class holds several children-objects of the abstract
  *                   class Visualisation.
  */
 public class UserInterface {
-	
+
 	private final String dataSource = "Berkeley Earth";
 	private final String sourceURL = "http://www.berkeleyearth.org/";
-	
+
 	private TabLayoutPanel tabs = new TabLayoutPanel(20, Unit.PT);;
 	private Visualisation map = new Map(Data.getRandomData(100));
 	private Visualisation table = new Table(Data.getRandomData(100));
-	// private FilterMenu filterMenu;
-	
+	private Visualisation filterMenu = new FilterMenu(Data.getRandomData(100));
+
 	/**
 	 * Creates the tab structured GUI and displays the data source information.
 	 * Requires an already existing TabLayoutPanel object called tabs.
@@ -38,32 +40,32 @@ public class UserInterface {
 	 * @return - 
 	 */
 	public void createGUI() {
-		
+
 		tabs.add(map.getPanel(), "Map");
 		tabs.add(table.getPanel(), "Table");
-		tabs.add(new Label("Filter"), "Filter");
-		
+		tabs.add(filterMenu.getPanel(), "Filter");
+
 		tabs.setAnimationDuration(450);
 		tabs.setAnimationVertical(false);
-		
+
 		RootPanel.get("tabContainer").add(tabs);
-		
+
 		tabs.setHeight("400px");
 		tabs.setAnimationDuration(1000);
-		
+
 		HorizontalPanel sourceContainer = new HorizontalPanel();
 		sourceContainer.addStyleName("sourceContainer");
-		
+
 		Label sourceInfo = new Label("Source of raw data:");
 		Anchor sourceLink = new Anchor(dataSource, sourceURL);
 		sourceLink.setTarget("_blank");
 		sourceInfo.addStyleName("sourceInfo");
-		
+
 		sourceContainer.add(sourceInfo);
 		sourceContainer.add(sourceLink);
-		
+
 		RootPanel.get("sourceContainer").add(sourceContainer);
-		
+
 	}
-	
+
 }
