f7d9cd95aab39e42d8381aa17d0e5b2650db61f3
Jonathan Stahl <joni@stahlfamily.ch>
2016-11-14 17:40:55 +0100

    created junit test for Table

---
+++ CliMeter/test/ch/uzh/ifi/rerg/se16_climeter/client/TableTest.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package ch.uzh.ifi.rerg.se16_climeter.client;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+/**
+ * @author Jonathan Stahl
+ *
+ */
+public class TableTest extends GWTTestCase {
+	
+	@Override
+	public String getModuleName() {
+		return "ch.uzh.ifi.rerg.se16_climeter.CliMeter";
+	}
+	
+
+	@Test
+	public void testTable() {
+		Table table = null;
+		
+		assertNotNull(table);
+	}
+
+	
+	@Test
+	public void testAddData() {
+		assertTrue(true);
+	}
+
+	
+
+}
