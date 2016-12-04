89a4f192b2e8856ec98740f26bf1556c790fcc52
Jonathan Stahl <joni@stahlfamily.ch>
2016-12-04 21:25:10 +0100

    continued FilterMenu

--- CliMeter/src/ch/uzh/ifi/rerg/se16_climeter/client/filtermenu/FilterMenu.java
+++ CliMeter/src/ch/uzh/ifi/rerg/se16_climeter/client/filtermenu/FilterMenu.java
@@ -1,9 +1,11 @@
 package ch.uzh.ifi.rerg.se16_climeter.client.filtermenu;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.CheckBox;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.MultiWordSuggestOracle;
 import com.google.gwt.user.client.ui.SuggestBox;
@@ -12,6 +14,7 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 
 import ch.uzh.ifi.rerg.se16_climeter.client.Data;
+import ch.uzh.ifi.rerg.se16_climeter.client.Filter;
 import ch.uzh.ifi.rerg.se16_climeter.client.Visualisation;
 import ch.uzh.ifi.rerg.se16_climeter.client.table.Table;
 
@@ -33,7 +36,16 @@ public class FilterMenu extends Visualisation {
 	String[] countryArray = {"Schweiz","Deutschland","Frankreich","Schweiz1","Schweiz2"};
 	String[] cityArray = {"Zürich1","Zürich2","Zürich3","Winterthur","Winterthur1"};
 	
-	Table table;
+	private Table table;
+	
+	private SuggestBox citySuggestBox;
+	private SuggestBox countrySuggestBox;
+	
+	private TextBox beginDateBox;
+	private TextBox endDateBox;
+	
+	private CheckBox inaccuracyCheckBox;
+	private TextBox inaccuracyBox;
 	
 
 	public FilterMenu(ArrayList<Data> data, Table table){
@@ -47,7 +59,7 @@ public class FilterMenu extends Visualisation {
 		filterMenuPanel.add(cityBox(data));
 		filterMenuPanel.add(addDateFilterPanel());	
 		filterMenuPanel.add(addInaccuracyPanel());	
-		filterMenuPanel.add(addResetButton());
+		filterMenuPanel.add(addButtons());
 
 		filterMenuPanel.setStyleName("filterMenuPanel");
 		panel.add(filterMenuPanel);
@@ -66,12 +78,12 @@ public class FilterMenu extends Visualisation {
 		}
 
 		// Create the suggest box
-		final SuggestBox suggestBox = new SuggestBox(oracle);
-		suggestBox.setStyleName("suggestBox");
+		countrySuggestBox = new SuggestBox(oracle);
+		countrySuggestBox.setStyleName("suggestBox");
 		VerticalPanel suggestPanel = new VerticalPanel();
 		suggestPanel.add(new Label("Country:"));
-		suggestPanel.add(suggestBox);
-		suggestPanel.add(addFilterButton());
+		suggestPanel.add(countrySuggestBox);
+		//suggestPanel.add(addFilterButton());
 		return suggestPanel;
 	}
 
@@ -86,12 +98,12 @@ public class FilterMenu extends Visualisation {
 		}
 
 		// Create the suggest box
-		final SuggestBox suggestBox = new SuggestBox(cityOracle);		
-		suggestBox.setStyleName("suggestBox");
+		citySuggestBox = new SuggestBox(cityOracle);		
+		citySuggestBox.setStyleName("suggestBox");
 		VerticalPanel suggestPanel = new VerticalPanel();
 		suggestPanel.add(new Label("City:"));
-		suggestPanel.add(suggestBox);
-		suggestPanel.add(addFilterButton());
+		suggestPanel.add(citySuggestBox);
+		//suggestPanel.add(addFilterButton());
 		return suggestPanel;
 	}
 
@@ -102,18 +114,28 @@ public class FilterMenu extends Visualisation {
 		return filterButton;
 	}
 
-	public Widget addResetButton() {
-
+	public Widget addButtons() {
+		
+		HorizontalPanel buttonPanel = new HorizontalPanel();
+		
+		Button applyButton = new Button("Apply");
+		applyButton.setStyleName("applyButton");
+		
 		Button resetButton = new Button("RESET");
 		resetButton.setStyleName("resetButton");
-		return resetButton;
+		
+		buttonPanel.add(applyButton);
+		buttonPanel.add(resetButton);
+		
+		
+		return buttonPanel;
 	}
 
 	public Widget addInaccuracyPanel() {
 
 		VerticalPanel inaccuracyPanel = new VerticalPanel();
-		CheckBox inaccuracyCheckBox = new CheckBox("Hide inaccurate data");
-		TextBox inaccuracyBox = new TextBox();
+		inaccuracyCheckBox = new CheckBox("Hide inaccurate data");
+		inaccuracyBox = new TextBox();
 		inaccuracyBox.setStyleName("inaccuracyBox");
 		inaccuracyPanel.add(inaccuracyCheckBox);
 		inaccuracyPanel.add(inaccuracyBox);
@@ -123,23 +145,41 @@ public class FilterMenu extends Visualisation {
 	public Widget addDateFilterPanel() {
 
 		VerticalPanel dateFilterPanel = new VerticalPanel();
+		
 		dateFilterPanel.add(new Label("Filter dates from: "));
-
-		TextBox dateFilterBoxFrom = new TextBox();
-		dateFilterBoxFrom.setStyleName("dateFilterBoxFrom");
-		dateFilterPanel.add(dateFilterBoxFrom);
+		beginDateBox = new TextBox();
+		beginDateBox.setStyleName("beginDateBox");
+		dateFilterPanel.add(beginDateBox);
 		
 		dateFilterPanel.add(new Label("To: "));
-		TextBox dateFilterBoxTo = new TextBox();
-		dateFilterBoxTo.setStyleName("dateFilterBoxTo");
-		dateFilterPanel.add(dateFilterBoxTo);
+		endDateBox = new TextBox();
+		endDateBox.setStyleName("endDateBox");
+		dateFilterPanel.add(endDateBox);
 
-		Button dateButton = new Button("Filter date");
-		dateButton.setStyleName("dateButton");
+		//Button dateButton = new Button("Filter date");
+		//dateButton.setStyleName("dateButton");
 
-		dateFilterPanel.add(dateButton);
+		//dateFilterPanel.add(dateButton);
 
 
 		return dateFilterPanel;
+	}
+	
+	public Filter getFilterValues(){
+		Filter filter = new Filter();
+		
+		String country = countrySuggestBox.getText();
+		String city = citySuggestBox.getText();
+		Date beginDate; // TODO!
+		Date endDate; //TODO!
+		
+		if (inaccuracyCheckBox.getValue() == true){
+			Double maxUncertainty = Double.parseDouble(inaccuracyBox.getText());
+		}
+		
+		
+		
+		
+		return filter;
 	}
 }
