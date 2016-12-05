57c24f11172fc879b8f8f1947c753bd8dae69bc3
timolex <timo.surbeck@gmail.com>
2016-12-05 16:07:08 +0100

    smaller fixes, TimeLineTest works again

--- CliMeter/test/ch/uzh/ifi/rerg/se16_climeter/client/timeline/TimeLineTest.java
+++ CliMeter/test/ch/uzh/ifi/rerg/se16_climeter/client/timeline/TimeLineTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 import com.google.gwt.junit.client.GWTTestCase;
 
+import ch.uzh.ifi.rerg.se16_climeter.client.map.MapComposite;
+
 public class TimeLineTest extends GWTTestCase {
 
 	@Override
@@ -13,13 +15,13 @@ public class TimeLineTest extends GWTTestCase {
 
 	@Test
 	public void testTimeLine_null() {
-		TimeLine timeLine = new TimeLine(1800, 2016);
+		TimeLine timeLine = new TimeLine(1800, 2016, new MapComposite());
 		assertNotNull(timeLine);
 	}
 
 	@Test
 	public void testTimeLine_1() {
-		TimeLine timeLine = new TimeLine(1800, 2016);
+		TimeLine timeLine = new TimeLine(1800, 2016, new MapComposite());
 		assertEquals(1800.0, timeLine.getMinValue());
 		assertEquals(2016.0, timeLine.getMaxValue());
 	}
